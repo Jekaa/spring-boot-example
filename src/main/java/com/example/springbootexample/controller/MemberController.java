6309cfd61408d385558a576bd051d4d9a10be0fb
Jekaa <1owa@mail.ru>
2019-03-28 09:39:28 +0700

    import

--- src/main/java/com/example/springbootexample/controller/MemberController.java
+++ src/main/java/com/example/springbootexample/controller/MemberController.java
@@ -6,7 +6,14 @@ import com.example.springbootexample.model.Member;
 import com.example.springbootexample.model.MembersGroup;
 import com.example.springbootexample.service.AgeFinder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
